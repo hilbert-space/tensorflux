118a91633a0cf27d79af150a0e584aa7b68f4a1d
Ivan Ukhov <ivan.ukhov@gmail.com>
2016-07-04 10:39:18 +0200

    Get rid of Code

--- src/error.rs
+++ src/error.rs
@@ -1,42 +1,21 @@
+use ffi::TF_Code;
 use std::ffi::CStr;
 use std::{error, fmt};
 
 use status::{self, Status};
 
-translate! {
-    pub struct Code => TF_Code,
-
-    OK => TF_OK,
-    Aborted => TF_ABORTED,
-    AlreadyExists => TF_ALREADY_EXISTS,
-    Cancelled => TF_CANCELLED,
-    DataLoss => TF_DATA_LOSS,
-    DeadlineExceeded => TF_DEADLINE_EXCEEDED,
-    FailedPrecondition => TF_FAILED_PRECONDITION,
-    Internal => TF_INTERNAL,
-    InvalidArgument => TF_INVALID_ARGUMENT,
-    NotFound => TF_NOT_FOUND,
-    OutOfRange => TF_OUT_OF_RANGE,
-    PermissionDenied => TF_PERMISSION_DENIED,
-    ResourceExhausted => TF_RESOURCE_EXHAUSTED,
-    Unauthenticated => TF_UNAUTHENTICATED,
-    Unavailable => TF_UNAVAILABLE,
-    Unimplemented => TF_UNIMPLEMENTED,
-    Unknown => TF_UNKNOWN,
-}
-
 /// An error.
 #[derive(Clone, Debug)]
 pub struct Error {
-    code: Code,
+    code: TF_Code,
     message: String,
 }
 
 impl Error {
     /// Return the current error if any.
     pub fn current(status: &Status) -> Option<Self> {
-        let code = Code::from(ffi!(TF_GetCode(status::as_raw(status))));
-        if code == Code::OK {
+        let code = ffi!(TF_GetCode(status::as_raw(status)));
+        if code == TF_Code::TF_OK {
             return None;
         }
         let message = ffi!(TF_Message(status::as_raw(status)));
@@ -68,6 +47,6 @@ impl error::Error for Error {
 impl<T> From<T> for Error where T: Into<String> {
     #[inline]
     fn from(message: T) -> Error {
-        Error { code: Code::Unknown, message: message.into() }
+        Error { code: TF_Code::TF_UNKNOWN, message: message.into() }
     }
 }
